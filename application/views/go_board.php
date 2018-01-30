1e5e4b4dab33517467a8328021ff77a829da5ad9
Florian Rambur <florian@manapani.com>
2018-01-30 13:28:49 +0100

    Correction coordonnées

--- application/views/go_board.php
+++ application/views/go_board.php
@@ -6,7 +6,7 @@
             for ($i = 0 ; $i<$size_goban ; $i++) {
                 $str .= '<tr>';
                 for ($j = 0 ; $j<$size_goban ; $j++) {
-                    $str .= '<td id="'.$i.'.'.$j.'" class="case"><i class="fa fa-lg fa-circle pre-stone black"></i></td>';
+                    $str .= '<td id="'.$i.'_'.$j.'" class="case"><i class="fa fa-lg fa-circle pre-stone black"></i></td>';
                 }
                 $str .= '</tr>';
             }
