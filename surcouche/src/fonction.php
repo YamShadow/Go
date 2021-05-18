67146c4fce4c5a59327e4c874a54949eed0a8465
2alheure <jojo77.juv@live.fr>
2021-05-18 22:04:19 +0200

    fix BASE_URL + curly braces

--- surcouche/src/fonction.php
+++ surcouche/src/fonction.php
@@ -6,17 +6,17 @@
 
 function conf_baseurl($prod, $git, $ssl){
     $url = $ssl ? 'https://' : 'http://';
-    if($prod{strlen($prod)-1} != '/')
+    if($prod[strlen($prod)-1] != '/')
         $prod = $prod.'/';
     if (ENVIRONMENT == 'production'){
-        if($prod{strlen($prod)-1} != '/')
+        if($prod[strlen($prod)-1] != '/')
             $prod = $prod.'/';
         return $url . $prod;
     }
 
     if( $_SERVER['SERVER_NAME'] == 'localhost')
         $url .= 'localhost/';
-    if($git{strlen($git)-1} != '/')
+    if($git[strlen($git)-1] != '/')
         $git = $git.'/';
     return $url . $git;
 }
